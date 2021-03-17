be854b90b90b21bcb6be94b2ebe518e97a1adb24
Claudio Noguera <claudio.noguera@gmail.com>
2021-03-17 22:42:00 +0100

    manually create 0.20.0

---
+++ Formula/dinero.rb
@@ -0,0 +1,11 @@
+class Dinero < Formula
+  desc "Command line tool for managing ledger files written in Rust"
+  homepage "https://github.com/frosklis/dinero-rs"
+  url "https://github.com/frosklis/dinero-rs/releases/latest/download/dinero-rs-mac.tar.gz"
+  sha256 "bc6f247647877953c91f0e59b841131ac25adebb53b316e2fc13a3099e8a9bbd"
+  version "0.20.0"
+  https://github.com/frosklis/dinero-rs/releases/latest/download/dinero-mac-x86_64.tar.gz
+  def install
+    bin.install "dinero"
+  end
+end
