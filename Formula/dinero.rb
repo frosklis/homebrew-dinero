97e5c0881b7161bc23757c10c957f383707c841b
Claudio Noguera <claudio.noguera@gmail.com>
2021-03-20 01:18:49 +0100

    detect OS

--- Formula/dinero.rb
+++ Formula/dinero.rb
@@ -1,9 +1,16 @@
 class Dinero < Formula
+  version "0.20.0"
   desc "Command line tool for managing ledger files written in Rust"
   homepage "https://github.com/frosklis/dinero-rs"
-  url "https://github.com/frosklis/dinero-rs/releases/latest/download/dinero-mac-x86_64.tar.gz"
-  sha256 "bc6f247647877953c91f0e59b841131ac25adebb53b316e2fc13a3099e8a9bbd"
-  version "0.20.0"
+  
+  if OS.mac?
+    if RUBY_PLATFORM.match(/x86_64/)
+      puts "Detected x86_64"
+      url "https://github.com/frosklis/dinero-rs/releases/latest/download/dinero-mac-x86_64.tar.gz"
+      sha256 "bc6f247647877953c91f0e59b841131ac25adebb53b316e2fc13a3099e8a9bbd"
+    end
+  end
+
   def install
     bin.install "dinero"
   end
